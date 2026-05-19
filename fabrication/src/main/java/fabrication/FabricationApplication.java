11421916e87495a6bb520cb7866663978304af3f
nesrine zarrouk <nesrinezarrouk83@gmail.com>
2026-05-19 22:32:35 +0100

    init projet springboot

---
+++ fabrication/src/main/java/fabrication/FabricationApplication.java
@@ -0,0 +1,13 @@
+package fabrication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricationApplication.class, args);
+	}
+
+}
---
+++ fabrication/src/test/java/fabrication/FabricationApplicationTests.java
@@ -0,0 +1,13 @@
+package fabrication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
